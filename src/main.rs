8d04ed756c51e570a833e1dab7a1e6b2a9a0c647
corocn <crsk.plmzh@gmail.com>
2021-01-10 23:31:27 +0900

    rust bookちょっと読みすすめた

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,21 @@
-use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
-
 fn main() {
-    println!("Guess the number!");
+    let x = 5;
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let condition = true;
+    let y = if condition { 5 } else { 6 };
 
-    // println!("The secret number is: {}", secret_number);
+    println!("{}", add(x, y));
 
-    loop {
-        println!("Please input your guess.");
+    println!("----");
 
-        let mut guess = String::new();
+    let num = [10, 20, 30, 40, 50];
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            },
-        }
+    for n in (10..20).rev() {
+        println!("{}", n)
     }
+}
+
+fn add(x: i32, y: i32) -> i32 {
+    println!("{} + {}", x, y);
+    x + y
 }
