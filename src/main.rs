4676f072c72ccaee346f491e7acbf8aa00126032
corocn <crsk.plmzh@gmail.com>
2021-01-24 21:37:15 +0900

    8.2まで

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,9 @@ struct Item2 {
 
 
 fn main() {
-    // let mut v = vec![100, 32, 57];
-    // for i in &mut v {
-    //     *i += 50;
-    // }
-    //
-    // for i in &v {
-    //     println!("{}", i);
-    // }
+    let hello = "Здравствуйте";
 
-    let v = vec![
-        Item { name: String::from("hoge1") },
-        Item { name: String::from("hoge2") },
-    ];
+    let s = &hello[0..4];
 
-    for item in &v {
-        println!("{}", item.name);
-    }
+    println!("{}", s);
 }
