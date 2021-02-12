0c91b29fbbb2d99bbee5fc4345f8a465d6892623
corocn <crsk.plmzh@gmail.com>
2021-02-12 22:40:19 +0900

    10.2

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,23 @@
-pub trait Summary {
-    fn summarize_author(&self) -> String;
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
 
-    fn summarize(&self) -> String {
-        format!("(Read more from {}...)", self.summarize_author())
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
     }
-}
 
-pub struct NewsArticle {
-    pub headline: String,
-    pub location: String,
-    pub author: String,
-    pub content: String,
-}
-
-impl Summary for NewsArticle {
-    fn summarize_author(&self) -> String {
-        format!("@{}", self.author)
-    }
-}
-
-pub trait Hoge1 {
-    fn say1(&self) -> String;
-}
-
-impl Hoge1 for NewsArticle {
-    fn say1(&self) -> String {
-        return String::from("ほげわん");
-    }
-}
-
-pub trait Hoge2 {
-    fn say2(&self) -> String;
-}
-
-impl Hoge2 for NewsArticle {
-    fn say2(&self) -> String {
-        return String::from("ほげつー");
-    }
-}
-
-pub fn notify<T: Summary>(item: &T) {
-    println!("Breaking news! {}", item.summarize());
-}
-
-pub fn test<T: Hoge1 + Hoge2>(item: &T) {
-    println!("{} {}", item.say1(), item.say2())
-}
-
-fn returns_hoge() -> impl Hoge1 + Hoge2 + Summary {
-    NewsArticle {
-        headline: String::from("Penguins win the Stanley Cup Championship!"),
-        location: String::from("Pittsburgh, PA, USA"),
-        author: String::from("Iceburgh"),
-        content: String::from(
-            "The Pittsburgh Penguins once again are the best \
-             hockey team in the NHL.",
-    )}
+    largest
 }
 
 fn main() {
-    let article = returns_hoge();
-    println!("New article available! {}", article.summarize());
+    let number_list = vec![34, 50, 25, 100, 65];
 
-    test(&article);
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 
-    notify(&article);
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 }
