8392fcd0fb3eac82c49ef19b309bd265ec76730d
corocn <crsk.plmzh@gmail.com>
2021-02-13 22:41:43 +0900

    10.3

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,12 @@
-fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
-    let mut largest = list[0];
-
-    for &item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
+struct ImportantExcerpt<'a> {
+    part: &'a str,
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {}", result);
+    // 僕をイシュマエルとお呼び。何年か前・・・
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.')
+        .next()
+        .expect("Could not find a '.'");  // '.'が見つかりませんでした
+    let i = ImportantExcerpt { part: first_sentence };
 }
