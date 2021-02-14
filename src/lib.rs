52cf14e1fb5f64b08decf1f7075bd6dcc8554ad9
corocn <crsk.plmzh@gmail.com>
2021-02-14 22:03:12 +0900

    11.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
     }
-}
 
-pub fn eat_at_restaurant() {
-    // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
-
-    // Relative path
-    front_of_house::hosting::add_to_waitlist();
+    #[test]
+    fn another() {
+        //このテストを失敗させる
+        panic!("Make this test fail");
+    }
 }
