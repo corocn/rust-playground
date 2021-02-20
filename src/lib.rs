eea4db6ec12299ac066b1c342bcf3741a9220ec8
corocn <crsk.plmzh@gmail.com>
2021-02-20 22:23:52 +0900

    fizzbuzz with test

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,26 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn exploration() {
-        assert_eq!(2 + 2, 4);
+pub fn fizzbuzz(n: i32) -> String {
+    if n % 3 == 0 && n % 5 == 0 {
+        return String::from("fizzbuzz")
+    } else if n % 3 == 0 {
+        return String::from("fizz");
+    } else if n % 5 == 0 {
+        return String::from("buzz");
     }
 
+    return n.to_string();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
     #[test]
-    fn another() {
-        //このテストを失敗させる
-        panic!("Make this test fail");
+    fn test1() {
+        assert_eq!("1", fizzbuzz(1));
+        assert_eq!("2", fizzbuzz(2));
+        assert_eq!("fizz", fizzbuzz(3));
+        assert_eq!("4", fizzbuzz(4));
+        assert_eq!("buzz", fizzbuzz(5));
+        assert_eq!("fizzbuzz", fizzbuzz(15));
     }
 }
