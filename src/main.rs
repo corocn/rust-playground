4fbf6da6dd8b2e2f92304f6f6db1b9b862eb8b02
corocn <crsk.plmzh@gmail.com>
2021-03-07 23:29:55 +0900

    closure

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,86 @@
-extern crate rust_playground;
+use std::thread;
+use std::time::Duration;
 
-use std::env;
-use std::process;
-
-use rust_playground::Config;
+fn simulated_expensive_calculation(intensity: u32) -> u32 {
+    println!("calculating slowly...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        // 引数解析時に問題
-        println!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
+    generate_workout(
+        simulated_user_specified_value,
+        simulated_random_number
+    );
+}
 
-    if let Err(e) = rust_playground::run(config) {
-        println!("Application error: {}", e);
+struct Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    calculation: T,
+    value: Option<u32>,
+}
 
-        process::exit(1);
+impl<T> Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            },
+        }
     }
 }
 
 
+fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!",
+            expensive_result.value(intensity)
+        );
+        println!(
+            "Next, do {} situps!",
+            expensive_result.value(intensity)
+        );
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expensive_result.value(intensity)
+            );
+        }
+    }
+}
+
+#[test]
+fn call_with_different_values() {
+    let mut c = Cacher::new(|a| a);
+
+    let v1 = c.value(1);
+    let v2 = c.value(2);
+
+    assert_eq!(v2, 2);
+}
