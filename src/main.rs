7f9aa8bda4c6434c599133f9202a61e4f1aa76a7
corocn <crsk.plmzh@gmail.com>
2021-03-08 22:58:39 +0900

    iterator

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,13 @@
 fn main() {
-}
+    let a = [1, 2, 3];
+    let b = [4, 5, 6];
 
-struct Counter {
-    count: u32,
-}
+    let mut i = a.iter().map(|x| x * x);
 
-impl Counter {
-    fn new() -> Counter {
-        Counter { count: 0 }
+    let next = i.next();
+
+    match next {
+        Some(n) => println!("{}", n),
+        None => println!("aaa")
     }
-}
-
-impl Iterator for Counter {
-    type Item = u32;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.count += 1;
-
-        if self.count < 6 {
-            Some(self.count)
-        } else {
-            None
-        }
-    }
-}
-
-#[test]
-fn calling_next_directly() {
-    let mut counter = Counter::new();
-
-    assert_eq!(counter.next(), Some(1));
-    assert_eq!(counter.next(), Some(2));
-    assert_eq!(counter.next(), Some(3));
-    assert_eq!(counter.next(), Some(4));
-    assert_eq!(counter.next(), Some(5));
-    assert_eq!(counter.next(), None);
 }
