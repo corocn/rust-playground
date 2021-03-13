abfa7250df4684f599f4e103c72d333b4fdb66d4
corocn <crsk.plmzh@gmail.com>
2021-03-13 17:01:31 +0900

    test mpsc

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
-enum List {
-    Cons(i32, Rc<List>),
-    Nil,
-}
-
-use List::{Cons, Nil};
-use std::rc::Rc;
+use std::thread;
+use std::sync::mpsc;
+use std::time::Duration;
 
 fn main() {
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    // a生成後のカウント = {}
-    println!("count after creating a = {}", Rc::strong_count(&a));
-    let b = Cons(3, Rc::clone(&a));
-    // b生成後のカウント = {}
-    println!("count after creating b = {}", Rc::strong_count(&a));
-    {
-        let c = Cons(4, Rc::clone(&a));
-        // c生成後のカウント = {}
-        println!("count after creating c = {}", Rc::strong_count(&a));
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        // 君のためにもっとメッセージを(more messages for you)
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        while true {
+            tx.send(String::from("hi")).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
     }
-    // cがスコープを抜けた後のカウント = {}
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
