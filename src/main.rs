8fbafcf3af12bf45f51433c18fc9fa85c9f688b8
corocn <crsk.plmzh@gmail.com>
2021-03-16 23:51:53 +0900

    lifetime annotation

--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,27 @@ impl Future for CountDown {
     }
 }
 
-fn main() {
-    let countdown_future1 = CountDown(10);
-    let countdown_future2 = CountDown(20);
-    let cd_set = join_all(vec![countdown_future1, countdown_future2]);
-    let res = executor::block_on(cd_set);
-    for (i, s) in res.iter().enumerate() {
-        println!("{}: {}", i, s);
+fn longest<'a, 'b>(x: &'a u32, y: &'b u32) -> &'b u32 where 'a: 'b {
+    if x > y {
+        x
+    } else {
+        y
     }
+}
+
+fn main() {
+    let a = 1;
+    let b = 2;
+
+    let x = longest( &a, &b);
+
+    println!("{}", x)
+
+    // let countdown_future1 = CountDown(10);
+    // let countdown_future2 = CountDown(20);
+    // let cd_set = join_all(vec![countdown_future1, countdown_future2]);
+    // let res = executor::block_on(cd_set);
+    // for (i, s) in res.iter().enumerate() {
+    //     println!("{}: {}", i, s);
+    // }
 }
