a0b4f5c55b02509b5b6eb364fdc0d7b4ea39be02
corocn <crsk.plmzh@gmail.com>
2021-03-16 22:58:44 +0900

    trait test

--- src/main.rs
+++ src/main.rs
@@ -16,21 +16,62 @@ fn error_handling(result: MyResult) -> MyResult {
     Ok(200)
 }
 
-fn main() {
-    // let result: Result<i32, String> = Ok(200);
-    let result: Result<i32, String> = Err(String::from("Hi"));
+struct Dog;
+struct Cat;
 
-    // match result {
-    //     Ok(code) => println!("code: {}", code),
-    //     Err(err) => println!("err: {}", err)
-    // }
+trait Tweet {
+    fn tweet(&self);
+}
 
-    let result= error_handling(result);
-
-    // let a = result.expect("hoge");
-    // println!("{}", a);
-
-    if let Err(err) = result {
-        println!("{}", err);
+impl Tweet for Dog {
+    fn tweet(&self) {
+        println!("wanwan!");
     }
+}
+
+impl Tweet for Cat {
+    fn tweet(&self) {
+        println!("nyanya!");
+    }
+}
+
+fn main() {
+    let dog = Dog {};
+    let cat = Cat {};
+
+    dog.tweet();
+    cat.tweet();
+
+    let animal_vec: Vec<Box<dyn Tweet>> = vec![Box::new(dog), Box::new(cat)];
+
+    for animal in animal_vec {
+        animal.tweet();
+    }
+
+    // let s = concat!("a", "b", 3);
+    // let s = format!("{}-{:?}", s, ("D", 5));
+    // println!("{}", s);
+    //
+    // println!("defined in file: {}", file!());
+    // println!("defined on line: {}", line!());
+    // println!("is test: {}", cfg!(unix));
+    // println!("CARGO_HOME: {}", env!("CARGO_HOME"));
+
+
+    // // let result: Result<i32, String> = Ok(200);
+    // let result: Result<i32, String> = Err(String::from("Hi"));
+    //
+    // // match result {
+    // //     Ok(code) => println!("code: {}", code),
+    // //     Err(err) => println!("err: {}", err)
+    // // }
+    //
+    // let result= error_handling(result);
+    //
+    // // let a = result.expect("hoge");
+    // // println!("{}", a);
+    //
+    // if let Err(err) = result {
+    //     println!("{}", err);
+    // }
 }
