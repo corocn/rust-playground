1148aa7302198b8d59600a724cf0417fa2395266
corocn <crsk.plmzh@gmail.com>
2021-03-26 21:49:10 +0900

    error handling

--- src/bin/err_panic.rs
+++ src/bin/err_panic.rs
@@ -1,12 +1,26 @@
-fn get_int_from_file() -> i32 {
+enum MyError {
+    Io(std::io::Error),
+    Num(std::num::ParseIntError)
+}
+
+fn get_int_from_file() -> Result<i32, MyError> {
     let path = "number.txt";
 
-    let num_str = std::fs::read_to_string(path).expect("failed to open the file.");
-    let ret = num_str.trim().parse::<i32>().expect("failed to parse string to a number.");
+    let num_str = std::fs::read_to_string(path).map_err(|e| MyError::Io(e))?;
 
-    ret * 2
+    num_str
+        .trim()
+        .parse::<i32>()
+        .map(|t| t * 2)
+        .map_err(|e| MyError::Num(e))
 }
 
 fn main() {
-    println!("{}", get_int_from_file());
+    match get_int_from_file() {
+        Ok(x) => println!("{}", x),
+        Err(e) => match e {
+            MyError::Io(cause) => println!("I/O Error: {}", cause),
+            MyError::Num(cause) => println!("Parse Error: {}", cause),
+        },
+    }
 }
