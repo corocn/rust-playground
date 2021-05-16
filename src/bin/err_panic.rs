7f9e7d7db821dc02466ea08360aa8d245e29d9f3
corocn <crsk.plmzh@gmail.com>
2021-05-16 22:31:10 +0900

    wip

--- src/bin/err_panic.rs
+++ src/bin/err_panic.rs
@@ -1,44 +1,21 @@
-use std::fmt;
+use anyhow::{Context, Result};
 
-enum MyError {
-    Io(std::io::Error),
-    Num(std::num::ParseIntError)
-}
-
-impl fmt::Display for MyError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            MyError::Io(cause) => write!(f, "I/O Error: {}", cause),
-            MyError::Num(cause) => write!(f, "Parse Error: {}", cause),
-        }
-    }
-}
-
-impl From<std::io::Error> for MyError {
-    fn from(cause: std::io::Error) -> Self {
-        Self::Io(cause)
-    }
-}
-
-fn get_int_from_file() -> Result<i32, MyError> {
+fn get_int_from_file() -> Result<i32> {
     let path = "number.txt";
 
-    let num_str = std::fs::read_to_string(path).map_err(MyError::from)?;
+    let num_str = std::fs::read_to_string(path)
+        .with_context(|| format!("failed to read string from {}", path))?;
 
     num_str
         .trim()
         .parse::<i32>()
         .map(|t| t * 2)
-        .map_err(|e| MyError::Num(e))
+        .context("failed to parse string")
 }
 
 fn main() {
     match get_int_from_file() {
         Ok(x) => println!("{}", x),
-        // Err(e) => match e {
-        //     MyError::Io(cause) => println!("I/O Error: {}", cause),
-        //     MyError::Num(cause) => println!("Parse Error: {}", cause),
-        // },
-        Err(e) => println!("{}", e)
+        Err(e) => println!("{:#?}", e)
     }
 }
