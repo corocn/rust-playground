054d34a104697bac389aee9b4a66e652a6484c58
corocn <crsk.plmzh@gmail.com>
2021-06-01 08:23:58 +0900

    rc

--- src/sort/bin/main.rs
+++ src/sort/bin/main.rs
@@ -1,14 +1,29 @@
-fn main() {
-    let mut A = [5, 2, 4, 6, 1, 3];
+use std::rc::Rc;
 
-    let len = A.len();
-    for j in 1..len {
-        let key = A[j];
-        let mut i = j;
-        while i > 0 && A[i] < A[i - 1] {
-            A.swap(i , i - 1);
-            i = i - 1;
-        }
+// fn insertion_sort() {
+//     let mut a = [5, 2, 4, 6, 1, 3];
+//
+//     let len = a.len();
+//     for j in 1..len {
+//         let key = a[j];
+//         let mut i = j;
+//         while i > 0 && a[i] < a[i - 1] {
+//             a.swap(i , i - 1);
+//             i = i - 1;
+//         }
+//     }
+//     dbg!(a);
+// }
+
+fn main() {
+    let a = Rc::new("hello".to_string());
+    println!("Reference Count of a: {}", Rc::strong_count(&a));
+
+    {
+        let b = Rc::clone(&a);
+        println!("Reference Count of a: {}", Rc::strong_count(&a));
+        println!("Reference Count of b: {}", Rc::strong_count(&b));
     }
-    dbg!(A);
+
+    println!("Reference Count of a: {}", Rc::strong_count(&a));
 }
