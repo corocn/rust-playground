48e8c39d17b62a020a554d1e857b06352c985db5
corocn <crsk.plmzh@gmail.com>
2021-05-31 23:54:32 +0900

    wip

---
+++ src/sort/bin/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let A = vec![5, 2, 4, 6, 1, 3];
+    let mut sorted: Vec<i32> = vec![];
+
+    let len = &A.len();
+    for j in 1..*len {
+        key = A[j];
+        let i = j - 1;
+
+        while i > 0 && A[i] > key {
+
+        }
+    }
+}
