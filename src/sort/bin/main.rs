bc4d4ce20f455f872ed273d5206912fdcc8187e3
corocn <crsk.plmzh@gmail.com>
2021-06-01 00:16:49 +0900

    insertion sort

--- src/sort/bin/main.rs
+++ src/sort/bin/main.rs
@@ -1,14 +1,14 @@
 fn main() {
-    let A = vec![5, 2, 4, 6, 1, 3];
-    let mut sorted: Vec<i32> = vec![];
+    let mut A = [5, 2, 4, 6, 1, 3];
 
-    let len = &A.len();
-    for j in 1..*len {
-        key = A[j];
-        let i = j - 1;
-
-        while i > 0 && A[i] > key {
-
+    let len = A.len();
+    for j in 1..len {
+        let key = A[j];
+        let mut i = j;
+        while i > 0 && A[i] < A[i - 1] {
+            A.swap(i , i - 1);
+            i = i - 1;
         }
     }
+    dbg!(A);
 }
